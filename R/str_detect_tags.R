c01a3b6c06489feedf53b7716c28b48e7ce82f87
Christopher Brown <cbrown@decisionpatterns.com>
2018-10-31 10:40:45 -0700

    Update str_detect_tags()

--- R/str_detect_tags.R
+++ R/str_detect_tags.R
@@ -11,7 +11,7 @@
 #' @param tags character; vector of one or more tags, can be PCRE
 #' @param sep character; single element (usually single character) separator
 #'   between tags in \code{string} ( Default: ',' )
-#' @param ignore.case logical whether to ignore case during matching
+#' @param ignore_case logical whether to ignore case during matching
 #'   ( Default: TRUE)
 #'
 #' Tags are matched using \code{str_detect} using Perl regular expressions.
@@ -27,8 +27,8 @@
 #'   string <- c( 'X', 'Y', 'Z', 'Y,Z' )
 #'   tags <- list( 'X', '(y)' )
 #'
-#'   str_detect_tag( string, tags )   # T, T, F
-#'   str_detect_tag( string, tags, ignore.case=FALSE )  # T, F, F
+#'   str_detect_tag( string, tags )   # T, T, F, T
+#'   str_detect_tag( string, tags, ignore_case=FALSE )  # T, F, F, F
 #'
 #' @note
 #'   allow for leading/trailing white space
@@ -36,7 +36,7 @@
 # @import stringr
 #' @export
 
-str_detect_tag <- function( string, tags, sep=',', ignore.case=TRUE ) {
+str_detect_tag <- function( string, tags, sep=',', ignore_case=TRUE ) {
 
   # Create pattern like: (^|,)(tags|to|match)(,|$)
 
@@ -48,8 +48,8 @@ str_detect_tag <- function( string, tags, sep=',', ignore.case=TRUE ) {
   pattern <- paste0( '(', pattern, ')' )
   pattern <- paste0( prefix, whitespace, pattern, whitespace, suffix )
 
-  if( ignore.case ) pattern <- ignore.case( pattern )
-  pattern <- perl( pattern )
+  # if( ignore_case ) pattern <- ignore_case( pattern )
+  pattern <- regex( pattern, ignore_case = ignore_case )
 
   str_detect( string, pattern )
 
