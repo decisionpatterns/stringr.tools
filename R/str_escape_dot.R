e7765928a04227b9867a1793a90354b3ae998d9e
Christopher Brown <cbrown@decisionpatterns.com>
2019-02-12 14:52:47 -0800

    v0.3.1: Fix str_escape_dot()

--- R/str_escape_dot.R
+++ R/str_escape_dot.R
@@ -6,14 +6,15 @@
 #'
 #' @details
 #'
-#' This escpaes **all** instances in single-dot in strings. This is handy when
-#' working with regular expressions where a period/dot is used to mean any
-#' character. By escaping, the regular expression will match on a literal
-#' period.
+#' `str_replace_dot` escapes **all** instances of single-dot in a string. This
+#' is handy when working with regular expressions where a period/dot is used to
+#' mean any character. By escaping, the regular expression will match on a
+#' literal period (`.`) instead. This is common in matching with filepaths.
 #'
 #' `str_replace_dot` is simply:
 #'
 #'      `str_replace_all(string, fixed("."), "\\.")``
+#'
 #'
 #' @examples
 #'   str_escape_dot("hello.world")   # "hello\\.world"
@@ -23,9 +24,4 @@
 #' @export
 
 str_escape_dot <- function(string)
-  str_replace_all::str_replace_all(string, fixed("."), "\\.")
-
-
-# OLD function
-# escape_dot <- function(string)
-#  str_replace(string, fixed("."), "\\.")
+  stringr::str_replace_all(string, fixed("."), "\\.")
